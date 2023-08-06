0e9980354a0bce0dda6de7aebf750ee911569b33
amcl11 <132857824+amcl11@users.noreply.github.com>
2023-08-06 18:52:26 +1000

    updated app.py code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -26,15 +26,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 #List all available api routes
-
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/home<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
     )
-    
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     
@@ -43,16 +42,15 @@ def precipitation():
     
     #Return precipitation data
     prcp_results = session.query(Measurement.date, Measurement.prcp).all()
-    session.close()
-
+    
     # Convert list of tuples into a dictionary with date as the key and prcp as the value
     precipitation_dict = {}
     for date, prcp in prcp_results:
         precipitation_dict[date] = prcp
 
-    return jsonify(precipitation_dict)
-
     session.close()
+    
+    return jsonify(precipitation_dict)
     
 @app.route("/api/v1.0/stations")
 def stations():
@@ -62,14 +60,13 @@ def stations():
     
     #Return station data
     station_results = session.query(Station.name).all()
-    session.close()
 
     # Convert list of tuples into a normal list of station names
     all_stations = [station[0] for station in station_results]
 
-    return jsonify(all_stations)
-
     session.close()
+    
+    return jsonify(all_stations)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -85,16 +82,59 @@ def tobs():
     
     #Return temp observations data
     yearly_temp_observations = session.query(Measurement.tobs).\
-        filter(Measurement.date >= one_year_ago, Measurement.station == most_active_station_id).all()
-
-    session.close()
+    filter(Measurement.date >= one_year_ago, Measurement.station == most_active_station_id).all()
     
     # Extract the temperature values from the list of tuples
     temperatures = [temp[0] for temp in yearly_temp_observations]
     
+    session.close()
+    
     return jsonify(temperatures)
 
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    
+    # Create a new session for this request
+    session = Session(engine)
+    
+    # Query TMIN, TAVG, and TMAX for all dates greater than or equal to the start date
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    
+    # Close the session after the request is processed
+    session.close()
+    
+    # Unpack the results into variables
+    tmin, tavg, tmax = results[0]
+    
+    # Return the JSON response
+    return jsonify({
+        "TMIN": tmin,
+        "TAVG": tavg,
+        "TMAX": tmax
+    })
 
-
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_by_date_range(start, end):
+    # Create a new session for this request
+    session = Session(engine)
+    
+    # Query TMIN, TAVG, and TMAX for dates in the range from start date to end date (inclusive)
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start, Measurement.date <= end).all()
+    
+    # Close the session after the request is processed
+    session.close()
+    
+    # Unpack the results into variables
+    tmin, tavg, tmax = results[0]
+    
+    # Return the JSON response
+    return jsonify({
+        "TMIN": tmin,
+        "TAVG": tavg,
+        "TMAX": tmax
+    })
+    
 if __name__ == '__main__':
     app.run(debug=True)
